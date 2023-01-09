b664c5cf9b19827318f442d5c76362a05c201da2
이환주_T4179 <82008727+Leehj0530@users.noreply.github.com>
2023-01-09 17:19:21 +0900

    update pre-processing

--- train.py
+++ train.py
@@ -23,6 +23,8 @@ def load_data():
     asdas = 'val/val_v1.0.json'
     with open() as f:
         data = json.load(f)
+        
+    return data
 
 
 @hydra.main(version_base="2.5", config_path=".", config_name="config.yaml")
@@ -61,7 +63,230 @@ def main(config):
 
 if __name__ == '__main__':
     main()
+    
+    
+import pandas as pd
+data = load_data()
+df = pd.DataFrame(data['data'])
 
+from datasets import Dataset
+dataset = Dataset.from_pandas(df.iloc[:50])
+
+from transformers import LayoutLMv2FeatureExtractor
+feature_extractor = LayoutLMv2FeatureExtractor()
+
+def get_ocr_words_and_boxes(examples):
+    from PIL import Image
+
+    root_dir = '/opt/ml/input/data/val/'
+    # get a batch of document images
+    images = [Image.open(root_dir + image_file).convert("RGB") for image_file in examples['image']]
+    
+    # resize every image to 224x224 + apply tesseract to get words + normalized boxes
+    encoded_inputs = feature_extractor(images)
+
+    examples['image'] = encoded_inputs.pixel_values
+    examples['words'] = encoded_inputs.words
+    examples['boxes'] = encoded_inputs.boxes
+
+    return examples
+
+## dataset_with_ocr = dataset.map(get_ocr_words_and_boxes, batched=True, batch_size=2)
+  
+
+def subfinder(words_list, answer_list):  
+    matches = []
+    start_indices = []
+    end_indices = []
+    for idx, i in enumerate(range(len(words_list))):
+        if words_list[i] == answer_list[0] and words_list[i:i+len(answer_list)] == answer_list:
+            matches.append(answer_list)
+            start_indices.append(idx)
+            end_indices.append(idx + len(answer_list) - 1)
+    if matches:
+        return matches[0], start_indices[0], end_indices[0]
+    else:
+        return None, 0, 0
+
+# example
+question = "where is it located?"
+words = ["this", "is", "located", "in", "the", "university", "of", "california", "in", "the", "US"]
+boxes = [[1000,1000,1000,1000] for _ in range(len(words))]
+answer = "university of california"
+
+
+from transformers import AutoTokenizer
+model_checkpoint = "microsoft/layoutlmv2-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+
+import transformers
+assert isinstance(tokenizer, transformers.PreTrainedTokenizerFast)
+
+# encode the example
+encoding = tokenizer(question, words, boxes=boxes)
+tokenizer.decode(encoding.input_ids)
+
+# find the answer in the words
+match, word_idx_start, word_idx_end = subfinder(words, answer.split())
+
+sequence_ids = encoding.sequence_ids()
+
+def get_start_end_token(sequence_ids):
+    # Start token index of the current span in the text.
+    token_start_index = 0
+    while sequence_ids[token_start_index] != 1:
+        token_start_index += 1
+
+    # End token index of the current span in the text.
+    token_end_index = len(encoding.input_ids) - 1
+    while sequence_ids[token_end_index] != 1:
+        token_end_index -= 1
+
+    word_ids = encoding.word_ids()[token_start_index:token_end_index+1]
+
+    for id in word_ids:
+        if id == word_idx_start:
+            start_position = token_start_index 
+        else:
+            token_start_index += 1
+
+    for id in word_ids[::-1]:
+        if id == word_idx_end:
+            end_position = token_end_index 
+        else:
+            token_end_index -= 1
+
+## ??
+# tokenizer.decode(encoding.input_ids[start_position:end_position+1])
+
+def encode_dataset(examples, max_length=512):
+    # take a batch 
+    questions = examples['question']
+    words = examples['words']
+    boxes = examples['boxes']
+
+    # encode it
+    encoding = tokenizer(questions, words, boxes, max_length=max_length, padding="max_length", truncation=True)
+
+    # next, add start_positions and end_positions
+    start_positions = []
+    end_positions = []
+    answers = examples['answers']
+    # for every example in the batch:
+    for batch_index in range(len(answers)):
+        cls_index = encoding.input_ids[batch_index].index(tokenizer.cls_token_id)
+        # try to find one of the answers in the context, return first match
+        words_example = [word.lower() for word in words[batch_index]]
+        for answer in answers[batch_index]:
+            match, word_idx_start, word_idx_end = subfinder(words_example, answer.lower().split())
+            if match:
+                break
+        # EXPERIMENT (to account for when OCR context and answer don't perfectly match):
+        if not match:
+            for answer in answers[batch_index]:
+                for i in range(len(answer)):
+                    # drop the ith character from the answer
+                    answer_i = answer[:i] + answer[i+1:]
+                    # check if we can find this one in the context
+                    match, word_idx_start, word_idx_end = subfinder(words_example, answer_i.lower().split())
+                    if match:
+                        break
+        # END OF EXPERIMENT 
+    
+        if match:
+            sequence_ids = encoding.sequence_ids(batch_index)
+            # Start token index of the current span in the text.
+            token_start_index = 0
+            while sequence_ids[token_start_index] != 1:
+                token_start_index += 1
+
+            # End token index of the current span in the text.
+            token_end_index = len(encoding.input_ids[batch_index]) - 1
+            while sequence_ids[token_end_index] != 1:
+                token_end_index -= 1
+        
+            word_ids = encoding.word_ids(batch_index)[token_start_index:token_end_index+1]
+            for id in word_ids:
+                if id == word_idx_start:
+                    start_positions.append(token_start_index)
+                    break
+                else:
+                    token_start_index += 1
+
+            for id in word_ids[::-1]:
+                if id == word_idx_end:
+                    end_positions.append(token_end_index)
+                    break
+                else:
+                    token_end_index -= 1
+        
+            start_position = start_positions[batch_index]
+            end_position = end_positions[batch_index]
+            reconstructed_answer = tokenizer.decode(encoding.input_ids[batch_index][start_position:end_position+1])
+        
+        else:
+            start_positions.append(cls_index)
+            end_positions.append(cls_index)
+  
+    encoding['image'] = examples['image']
+    encoding['start_positions'] = start_positions
+    encoding['end_positions'] = end_positions
+
+    return encoding
+
+
+
+from datasets import Features, Sequence, Value, Array2D, Array3D
+
+# we need to define custom features
+features = Features({
+    'input_ids': Sequence(feature=Value(dtype='int64')),
+    'bbox': Array2D(dtype="int64", shape=(512, 4)),
+    'attention_mask': Sequence(Value(dtype='int64')),
+    'token_type_ids': Sequence(Value(dtype='int64')),
+    'image': Array3D(dtype="int64", shape=(3, 224, 224)),
+    'start_positions': Value(dtype='int64'),
+    'end_positions': Value(dtype='int64'),
+})
+
+dataset_with_ocr = dataset.map(get_ocr_words_and_boxes, batched=True, batch_size=2)
+
+encoded_dataset = dataset_with_ocr.map(encode_dataset, batched=True, batch_size=2, 
+                                       remove_columns=dataset_with_ocr.column_names,
+                                       features=features)
+
+from PIL import Image
+
+idx = 44
+tokenizer.decode(encoded_dataset['input_ids'][idx])
+
+image = Image.open('/opt/ml/input/data/val/' + dataset['image'][idx])
+
+start_position = encoded_dataset['start_positions'][idx]
+end_position = encoded_dataset['end_positions'][idx]
+if start_position != 0:
+  print(tokenizer.decode(encoded_dataset['input_ids'][idx][start_position: end_position+1]))
+else:
+  print("Answer not found in context")
+
+
+import torch
+encoded_dataset.set_format(type="torch")
+dataloader = torch.utils.data.DataLoader(encoded_dataset, batch_size=4)
+batch = next(iter(dataloader))
+
+idx = 2
+
+tokenizer.decode(batch['input_ids'][2])
+
+start_position = batch['start_positions'][idx].item()
+end_position = batch['end_positions'][idx].item()
+
+tokenizer.decode(batch['input_ids'][idx][start_position:end_position+1])
+
+    
+    
+    
 class Trainer():
     def __init__(self, model, optimizer, config, train_data_loader, valid_data_loader, device):
         self.model = model
