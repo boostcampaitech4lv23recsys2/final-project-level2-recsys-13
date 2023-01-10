0a4d4bc717ea86f5a397f5d4d3746ec59ea42616
tcha <hataeck@gmail.com>
2023-01-10 15:55:28 +0000

    etc

--- train.py
+++ train.py
@@ -3,14 +3,16 @@ from transformers import AutoModelForQuestionAnswering
 from utils.util import set_seed
 from trainers.trainer import Trainer
 from data_loader.data_loaders import DataLoader
+# export HYDRA_FULL_ERROR=1
+
 
 @hydra.main(version_base="2.5", config_path=".", config_name="config.yaml")
 def main(config):
     # seed
     set_seed(config.seed)
-    
+
     model = AutoModelForQuestionAnswering.from_pretrained(config.checkpoint)
-    
+
     # build optimizer, learning rate scheduler. delete every lines containing lr_scheduler for disabling scheduler
     from transformers import AdamW
     optimizer = AdamW(model.parameters(), lr=config.lr)
@@ -24,10 +26,10 @@ def main(config):
                       valid_data_loader=data_loader.valid_data_loader,
                       device=config.device
                       )
-    
+
     trainer.train()
-    #trainer.validate()
-    trainer.inference(10, config)
+    # trainer.validate()
+    # trainer.inference(10, config)
 
 
 if __name__ == '__main__':
